9ac71d2a8d0727972015320d1eb777ecc024aef0
webistomin <webistomin@gmail.com>
2019-03-08 20:58:58 +0300

    Добавил скрипты

---
+++ makeblock.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+touch src/sass/blocks/$1.sass
+touch src/views/blocks/$1.pug
+mkdir src/img/block/$1
+
+echo -e "@import 'blocks/$1'" >> src/sass/main.sass
+echo -e ".$1" >> src/sass/blocks/$1.sass
+
+echo "Блок $1 создан"
