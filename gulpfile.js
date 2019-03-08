fb2843031247d37d8202855776a5d7d5d2c994b1
webistomin <webistomin@gmail.com>
2019-03-08 16:51:46 +0300

    Пишу сборку

---
+++ gulpfile.js
@@ -0,0 +1,96 @@
+"use strict";
+
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const plumber = require('gulp-plumber');
+const notify = require('gulp-notify');
+const autoprefixer = require('gulp-autoprefixer');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const pug = require('gulp-pug');
+const del = require('del');
+const runSequence = require('run-sequence');
+const gcmq = require('gulp-group-css-media-queries');
+const cleanCSS = require('gulp-clean-css');
+const chalk = require('chalk');
+
+gulp.task('clean:build', function() {
+  return del('./build');
+});
+
+gulp.task('server',  function() {
+  browserSync.init({
+    server: {baseDir: './build/'}
+  });
+
+  gulp.watch('src/views/**/*.*', ['pug']);
+  gulp.watch('src/sass/**/*.sass', ['sass']);
+  gulp.watch('src/js/**/*.js', ['copy:js']);
+  gulp.watch('src/img/**/*.*', ['copy:img']);
+});
+
+gulp.task('copy:js', function() {
+  return gulp.src('src/js/**/*.*')
+    .pipe(gulp.dest('./build/js'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('copy:img', function() {
+  return gulp.src('src/img/**/*.*')
+    .pipe(gulp.dest('./build/img'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('sass', function() {
+  return gulp.src('./src/sass/main.sass')
+    .pipe(plumber({
+      errorHandler: notify.onError(function(err){
+        return {
+          title: 'Styles',
+          message: err.message
+        }
+      })
+    }))
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 6 versions'],
+      cascade: false
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gcmq())
+    .pipe(cleanCSS({debug: true}, (details) => {
+      setTimeout(() => {
+        console.log(chalk.blue(`${details.stats.originalSize}KB – original CSS`));
+        console.log(chalk.blue(`${details.stats.minifiedSize}KB – minified CSS`));
+      }, 2000)
+    }))
+    .pipe(gulp.dest('./build/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('pug', function() {
+  return gulp.src('./src/views/pages/**/*.pug')
+    .pipe(plumber({
+      errorHandler: notify.onError(function(err){
+        return {
+          title: 'Pug',
+          message: err.message
+        }
+      })
+    }))
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('./build'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('default', function(){
+  runSequence(
+    'clean:build',
+    ['sass', 'pug', 'copy:js', 'copy:img',],
+    'server',
+  )
+});
+
