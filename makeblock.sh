3863ebd3dcbbbd04088543636f50644f4a849c95
webistomin <webistomin@gmail.com>
2019-03-09 08:22:55 +0300

    Update 2019-03-09T05:22:50.355Z

--- makeblock.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-touch src/sass/blocks/$1.sass
-touch src/views/blocks/$1.pug
-mkdir src/img/blocks/$1
-
-echo -e "@import 'blocks/$1'" >> src/sass/main.sass
-echo -e ".$1" >> src/sass/blocks/$1.sass
-
-echo "Блок $1 создан"
