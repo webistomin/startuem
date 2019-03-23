e53f3f52852e4aee4f5b67d88ef96abbec39c210
webistomin <webistomin@gmail.com>
2019-03-23 12:21:14 +0300

    1

--- gulp/tasks/prod/img.js
+++ gulp/tasks/prod/img.js
@@ -2,11 +2,10 @@ const gulp = require('gulp');
 const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
 const imagemin = require('gulp-imagemin');
-const imageminWebp = require('imagemin-webp');
 
 gulp.task('img:build', function() {
 
-  return gulp.src('src/img/**/*.+(jpg|jpeg|gif|png|webp|svg)')
+  return gulp.src('src/img/**/*.+(jpg|jpeg|gif|png|webp|svg)', '!src/img/favicon/*')
     .pipe(plumber({
       errorHandler: notify.onError(function(err) {
 
@@ -21,7 +20,8 @@ gulp.task('img:build', function() {
       imagemin.gifsicle({ interlaced: true }),
       imagemin.jpegtran({ progressive: true }),
       imagemin.optipng({ optimizationLevel: 5 }),
-      imagemin.svgo()]))
+      imagemin.svgo(),
+    ]))
     .pipe(gulp.dest('./build/img'));
 
 });
