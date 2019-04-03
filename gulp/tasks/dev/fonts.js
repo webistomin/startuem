1eeba4a2abe7d04a48e230b4f38b52761638a1a3
Alexey Istomin <webistomin@gmail.com>
2019-04-03 17:34:22 +0300

    Update fonts.js

--- gulp/tasks/dev/fonts.js
+++ gulp/tasks/dev/fonts.js
@@ -3,7 +3,7 @@ const browserSync = require('browser-sync').create();
 
 gulp.task('copy:fonts', function() {
 
-  gulp.src('src/fonts/**/*.{woff, woff2}')
+  gulp.src('src/fonts/**/*.{woff,woff2}')
     .pipe(gulp.dest('build/fonts'))
     .pipe(browserSync.stream());
 
