d5f038f1416894a5ccaff7666a969dbf5532436c
Alexey Istomin <webistomin@gmail.com>
2019-06-28 12:53:53 +0300

    Update ie11Pollyfils.js

--- src/js/lib/ie11Pollyfils.js
+++ src/js/lib/ie11Pollyfils.js
@@ -3,7 +3,7 @@ if ('NodeList' in window && !NodeList.prototype.forEach) {
   NodeList.prototype.forEach = function(callback, thisArg) {
 
     thisArg = thisArg || window;
-    for (let i = 0; i < this.length; i++) {
+    for (var i = 0; i < this.length; i++) {
 
       callback.call(thisArg, this[i], i, this);
 
